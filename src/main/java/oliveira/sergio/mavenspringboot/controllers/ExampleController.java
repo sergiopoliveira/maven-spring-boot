ee3c60ae34ed2271ec7fe1ca7aee62d58481e091
sergiopoliveira <sergiopoliveira@outlook.com>
2019-02-26 22:00:34 +0000

    it tests

--- src/main/java/oliveira/sergio/mavenspringboot/controllers/ExampleController.java
+++ src/main/java/oliveira/sergio/mavenspringboot/controllers/ExampleController.java
@@ -1,4 +1,19 @@
 package oliveira.sergio.mavenspringboot.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class ExampleController {
+
+    @RequestMapping("/")
+    public Map<String, Object> getIndex() {
+
+        Map<String, Object> returnval = new HashMap<>();
+        returnval.put("hello", "world");
+        return returnval;
+    }
 }
